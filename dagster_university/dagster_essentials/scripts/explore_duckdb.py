ce689f02d6d1b64a122b221b22f3b1851157163b
RMorsch-MontageMarketing <rmorsch@montagemarketinggroup.com>
2026-05-02 21:06:04 -0400

    [feat] Up to paritions

---
+++ dagster_university/dagster_essentials/scripts/explore_duckdb.py
@@ -0,0 +1,38 @@
+import duckdb
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # picks up .env from project root
+
+conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+
+"""
+    Queries
+"""
+trips_select_query = """
+    select * from trips
+    limit 5
+"""
+
+trips_by_week_query = """
+    select
+        date_trunc('week', pickup_datetime + INTERVAL '1 day') - INTERVAL '1 day' as period,
+        count(*) as num_trips,
+        sum(passenger_count) as passenger_count,
+        sum(total_amount) as total_amount,
+        sum(trip_distance) as trip_distance 
+    from trips
+    group by period
+    order by period
+"""
+
+trips_by_week_TEST_query = """
+    select *
+    from trips
+"""
+
+
+print(conn.execute("SHOW TABLES").df())
+print(conn.execute(trips_select_query).df())
+#print(df.info())
+
